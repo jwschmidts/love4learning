963c3bfdf409f9819785e0de30c45becb42101ea
John Schmidt <john@dgspro.com>
2017-04-08 21:13:05 -0600

    Updates

--- registration.php
+++ registration.php
@@ -1,44 +1,106 @@
+<?php
+$var = $_GET;
+?>
 <h1>Student Registration Form</h1>
 <form action="/l4l/registration_action.php" method="Get">
 <h2>Student Information</h2>
 First name:
-<input name="fname" type="text" value="<?php echo $var['fname']; ?>" />
+<input name="fname" type="text" value="<?php echo $var['fname']; ?>" required/>
 
 Last name:
-<input name="lname" type="text" value="<?php echo $var['lname']; ?>" />
+<input name="lname" type="text" value="<?php echo $var['lname']; ?>" required/>
 
 Name that student goes by:
-<input name="nickName" type="text" value="<?php echo $var['nickName']; ?>" />
+<input name="nickName" type="text" value="<?php echo $var['nickName']; ?>" required/>
 
 School Year:
-<input name="schoolYear" type="text" value="<?php echo $var['schoolYear']; ?>" />
+<input name="schoolYear" type="text" value="<?php echo $var['schoolYear']; ?>" required/>
 
-Two Day Session<input name="SessionType" type="radio" value="2Day" />        Three Day Session<input name="SessionType" type="radio" value="3Day" />
+Two Day Session<input name="SessionType" type="radio" value="2Day" required/>        Three Day Session<input name="SessionType" type="radio" value="3Day" required/>
 
 Date of Birth:
-<input name="DateOfBirth" type="text" value="<?php echo $var['DateOfBirth']; ?>" />
+<select name="BMonth" required>
+  <option selected disabled>Month</option>
+  <option value='January'>January</option>
+  <option value='February'>February</option>
+  <option value='March'>March</option>
+  <option value='April'>April</option>
+  <option value='May'>May</option>
+  <option value='June'>June</option>
+  <option value='July'>July</option>
+  <option value='August'>August</option>
+  <option value='September'>September</option>
+  <option value='October'>October</option>
+  <option value='November'>November</option>
+  <option value='December'>December</option>
+</select>
+<select name="BDay" required>
+  <option selected disabled>Month</option>
+  <option value='1'>1</option>
+  <option value='2'>2</option>
+  <option value='3'>3</option>
+  <option value='4'>4</option>
+  <option value='5'>5</option>
+  <option value='6'>6</option>
+  <option value='7'>7</option>
+  <option value='8'>8</option>
+  <option value='9'>9</option>
+  <option value='10'>10</option>
+  <option value='11'>11</option>
+  <option value='12'>12</option>
+  <option value='13'>13</option>
+  <option value='14'>14</option>
+  <option value='15'>15</option>
+  <option value='16'>16</option>
+  <option value='17'>17</option>
+  <option value='18'>18</option>
+  <option value='19'>19</option>
+  <option value='20'>20</option>
+  <option value='21'>21</option>
+  <option value='22'>22</option>
+  <option value='23'>23</option>
+  <option value='24'>24</option>
+  <option value='25'>25</option>
+  <option value='26'>26</option>
+  <option value='27'>27</option>
+  <option value='28'>28</option>
+  <option value='29'>29</option>
+  <option value='30'>30</option>
+  <option value='31'>31</option>
+</select>
+<select name="BYear" required>
+  <option selected disabled>Year</option>
+<?php
+$currYear = date('Y');
+for ($i=2;$i<7;$i++)
+{
+  $d = $currYear - $i;
+  echo "<option value='$d'>$d</option>";
+}
+?>
+</select>
 
 Age of Child:
-<input name="Age" type="text" value="<?php echo $var['Age']; ?>" />
+<input name="Age" type="text" value="<?php echo $var['Age']; ?>" required/>
 
 Age of Child after first day of Preschool:
-<input name="AgeFirstDay" type="text" value="<?php echo $var['AgeFirstDay']; ?>" />
+<input name="AgeFirstDay" type="text" value="<?php echo $var['AgeFirstDay']; ?>" required/>
 
-Boy <input name="Gender" type="radio" value="Boy" />                       Girl <input name="Gender" type="radio" value="Girl" />
+Boy <input name="Gender" type="radio" value="Boy" required/>                       Girl <input name="Gender" type="radio" value="Girl" />
 
-Right Handed<input name="DominantHand" type="radio" value="RightHand" />      Left Handed <input name="DominantHand" type="radio" value="LeftHand" />
+Right Handed<input name="DominantHand" type="radio" value="RightHand" required/>      Left Handed <input name="DominantHand" type="radio" value="LeftHand" />
 
 Additional Information about the child (i.e. food allergies, health issues, etc.):
 <input name="AdditionalInfo" type="text" value="<?php echo $var['AdditionalInfo']; ?>" />
 
 How many years would you like your child to attend Love 4 Learning Preschool?
 
-One Year<input name="LengthInSchool" type="radio" value="1" />            Two Years<input name="LengthInSchool" type="radio" value="2" />
+One Year<input name="LengthInSchool" type="radio" value="1" required/>            Two Years<input name="LengthInSchool" type="radio" value="2" />
 <h2>Parent Information</h2>
 Parent #1:
-First Name <input name="ParentFirstName1" type="text" value="<?php echo $var['ParentFirstName1']; ?>" />
+First Name <input name="ParentFirstName1" type="text" value="<?php echo $var['ParentFirstName1']; ?>" required/>
 
-Last Name <input name="ParentLastName1" type="text" value="<?php echo $var['ParentLastName1']; ?>" />
+Last Name <input name="ParentLastName1" type="text" value="<?php echo $var['ParentLastName1']; ?>" required/>
 
 Parent #2: (Optional)
 First Name <input name="ParentFirstName2" type="text" value="<?php echo $var['ParentFirstName2']; ?>" />
@@ -46,59 +108,59 @@ First Name <input name="ParentFirstName2" type="text" value="<?php echo $var['Pa
 Last Name <input name="ParentLastName2" type="text" value="<?php echo $var['ParentLastName2']; ?>" />
 
 Phone (home):
-<input name="Phone" type="text" value="<?php echo $var['Phone']; ?>" />
+<input name="Phone" type="text" value="<?php echo $var['Phone']; ?>" required/>
 (work)
 <input name="WorkPhone" type="text" value="<?php echo $var['WorkPhone']; ?>" />
 (cell)
 <input name="CellPhone" type="text" value="<?php echo $var['CellPhone']; ?>" />
 
 E-Mail:
-<input name="Email" type="text" value="<?php echo $var['Email']; ?>" />
+<input name="Email" type="email" value="<?php echo $var['Email']; ?>" required/>
 
-<input name="CheckEmail" type="checkbox" value="CheckEmail" /> Do you check email?
+Do you regularly check email?  &nbsp;  Yes <input name="CheckEmail" type="radio" value="Yes" /> &nbsp;   No <input name="CheckEmail" type="radio" value="No" />
 
 Address:
-<input name="Address" type="text" value="<?php echo $var['Address']; ?>" />
+<input name="Address" type="text" value="<?php echo $var['Address']; ?>" required/>
 
 City:
-<input name="City" type="text" value="<?php echo $var['City']; ?>" />
+<input name="City" type="text" value="<?php echo $var['City']; ?>" required/>
 
 State:
-<input name="State" type="text" value="<?php echo $var['State']; ?>" />
+<input name="State" type="text" value="<?php echo $var['State']; ?>" required/>
 
 Zip Code:
-<input name="Zip" type="text" value="<?php echo $var['Zip']; ?>" />
+<input name="Zip" type="text" value="<?php echo $var['Zip']; ?>" required/>
 
 Hobbies or professions you would be willing to share with the class (i.e. dentist, policeman, foreign country, etc.):
 <input name="Hobbies" type="text" value="<?php echo $var['Hobbies']; ?>" />
 <h2>Emergency Contact</h2>
 First Name:
-<input name="EmergencyFirstName" type="text" value="<?php echo $var['EmergencyFirstName']; ?>" />
+<input name="EmergencyFirstName" type="text" value="<?php echo $var['EmergencyFirstName']; ?>" required/>
 
 Last Name:
-<input name="EmergencyLastName" type="text" value="<?php echo $var['EmergencyLastName']; ?>" />
+<input name="EmergencyLastName" type="text" value="<?php echo $var['EmergencyLastName']; ?>" required/>
 
 Relationship:
-<input name="EmergencyRelationship" type="text" value="<?php echo $var['EmergencyRelationship']; ?>" />
+<input name="EmergencyRelationship" type="text" value="<?php echo $var['EmergencyRelationship']; ?>" required/>
 
 Phone:
-<input name="EmergencyPhone" type="text" value="<?php echo $var['EmergencyPhone']; ?>" />
+<input name="EmergencyPhone" type="text" value="<?php echo $var['EmergencyPhone']; ?>" required/>
 
 Address:
-<input name="EmergencyAddress" type="text" value="<?php echo $var['EmergencyAddress']; ?>" />
+<input name="EmergencyAddress" type="text" value="<?php echo $var['EmergencyAddress']; ?>" required/>
 
 City:
-<input name="EmergencyCity" type="text" value="<?php echo $var['EmergencyCity']; ?>" />
+<input name="EmergencyCity" type="text" value="<?php echo $var['EmergencyCity']; ?>" required/>
 
 State:
-<input name="EmergencyState" type="text" value="<?php echo $var['EmergencyState']; ?>" />
+<input name="EmergencyState" type="text" value="<?php echo $var['EmergencyState']; ?>" required/>
 
 Zip Code:
-<input name="EmergencyZip" type="text" value="<?php echo $var['EmergencyZip']; ?>" />
+<input name="EmergencyZip" type="text" value="<?php echo $var['EmergencyZip']; ?>" required/>
 <h2>I Understand (Please check box)</h2>
-<input name="PottyTrained" type="checkbox" value="PottyTrained" /> My child must be 3 by July 1st and is FULLY potty trained
+<input name="PottyTrained" type="checkbox" required/> My child must be 3 by July 1st and is FULLY potty trained
 
-I, <input style="width: 100px;" name="WavierName" type="text" value="<?php echo $var['WaiverName']; ?>" /> (name parent or legal gaurding), for myself and for my child, agree to all of the following.
+I, <input style="width: 100px;" name="WavierName" type="text" value="<?php echo $var['WaiverName']; ?>" required/> (name parent or legal gaurding), for myself and for my child, agree to all of the following.
 <ol>
  	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
  	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
@@ -108,15 +170,13 @@ I, <input style="width: 100px;" name="WavierName" type="text" value="<?php echo 
 </ol>
 (Please Check all boxes that apply)
 
-<input name="PhysicalUnaware" type="checkbox" value="checked" /> I am not aware of any physical or other condition which
-may affect my child
-<input name="WaiverChildNameUnware" type="text" value="<?php echo $var['WaiverChildNameUnware']; ?>" /> ability to participate in any activity at Love 4 Learning Preschool
+<input name="Physical" type="radio" value="unaware" /> I am not aware of any physical or other condition which
+may affect my child's ability to participate in any activity at Love 4 Learning Preschool
 
-<input name="PhysicalAware" type="checkbox" value="checked" /> I am aware my child's
-<input name="WaiverChildNameAware" type="text" value="<?php echo $var['WaiverChildNameAware']; ?>" /> suffers from the following physical or other conditions
-which may affect their involvement at Love 4 Learning Preschool:
-(List any physical impairments of any child who will be
-participating in Love 4 Learning Preschool classes.)
+<input name="Physical" type="radio" value="aware" /> I am aware my child suffers from the following physical or other conditions which may affect their involvement at Love 4 Learning Preschool:
+(List any physical impairments of any child who will be participating in Love 4 Learning Preschool classes.)
+<input name="WaiverChildNameAware" type="text" value="<?php echo $var['WaiverChildNameAware']; ?>" />
+
 <ol start="6">
  	<li>I agree not to hold Love 4 Learning Preschool, INC as it's entirety
 responsible for any injuries suffered by my child while involved
@@ -132,7 +192,7 @@ Parent/Guardian Authorization
 
 Please list any restrictions to permission of the following:
 
-<input name="Bus" type="checkbox" /> My child may be taken on field trips or excursions by bus or
+<input name="FieldTrip" type="checkbox" /> My child may be taken on field trips or excursions by bus or
 private motor vehicle, as well as on neighborhood walking
 excursions under required supervision
 
@@ -140,8 +200,8 @@ excursions under required supervision
 activities under required supervision.
 
 My child may be photographed for publicity or news purposes
-<input name="vehicle" type="checkbox" value="OnSite" /> on-site
-<input name="vehicle" type="checkbox" value="OffSite" /> off-site
+<input name="PhotographOnSite" type="checkbox" /> on-site
+<input name="PhotographOffSite" type="checkbox"  /> off-site
 
 <input name="Medicine" type="checkbox" />My child may be given non-prescribed medication as indicated
 on the container. This may include sunscreen, children's pain
@@ -166,12 +226,9 @@ WAIVER, PROMISE NOT TO SUE, AND A HOLD HARMLESS FOR ALL CLAIMS.
 THIS AGREEMENT SHALL BE BINDING UPON MYSELF, MY CHILD, AND
 OUR ESTATE, SUCCESSORS AND ASSIGNS.
 
-<input name="Sign" type="checkbox" value="1" /> By checking this box I agree that I have filled out this form to the best of my ability and agree with all that has been stated.
+<input name="Sign" type="checkbox" value="1" required/> By checking this box I agree that I have filled out this form to the best of my ability and agree with all that has been stated.
 Parent/Gaurdian Printed Name:
-<input name="PrintedName" type="text" value="<?php echo $var['PrintedName']; ?>" />
-
-Date:
-<input name="Date" type="text" value="<?php echo $var['Date']; ?>" />
+<input name="PrintedName" type="text" value="<?php echo $var['PrintedName']; ?>" required/>
 
 <input type="submit" value="Submit" />
 
--- registration_action.php
+++ registration_action.php
@@ -3,6 +3,14 @@
 include_once "sql.php";
 $variables = $_GET;
 
+$bday   = $variables['BDay'];
+$bmonth = $variables['BMonth'];
+$byear  = $variables['BYear'];
+
+$dob = strtotime("$bday  $bmonth  $byear");
+$dob = date('Y-m-d', $dob);
+
+//echo "<pre>"; print_r($variables); exit;
 $parentFirstName1 = sql_safe($variables['ParentFirstName1']);
 $parentLastName1  = sql_safe($variables['ParentLastName1']);
 $parentFirstName2 = sql_safe($variables['ParentFirstName2']);
@@ -11,7 +19,7 @@ $email            = sql_safe($variables['Email']);
 $phone            = (int)$variables['Phone'];
 $wPhone           = (int)$variables['WorkPhone'];
 $cPhone           = (int)$variables['CellPhone'];
-$checkEmail       = $variables['CheckEmail'] == 'CheckEmail' ? 1 : 0;
+$checkEmail       = $variables['CheckEmail'] == 'Yes' ? 1 : 0;
 $address          = sql_safe($variables['Address']);
 $city             = sql_safe($variables['City']);
 $state            = sql_safe($variables['State']);
@@ -20,22 +28,22 @@ $hobbies          = sql_safe($variables['Hobbies']);
 $fname            = sql_safe($variables['fname']);
 $lname            = sql_safe($variables['lname']);
 $nickName         = sql_safe($variables['nickName']);
-$dob              = strtotime($variables['DateOfBirth']);
 $schoolYear       = (int)$variables['schoolYear'];
 $gender           = $variables['Gender'] == 'Boy' ? 1 : 0;
 $dominantHand     = $variables['DominantHand'] == 'RightHand' ? 1 : 0;
 $additionalInfo   = sql_safe($variables['AdditionalInfo']);
+$physical         = sql_safe($variables['WaiverChildNameAware']);
 $lengthInSchool   = (int)$variables['LengthInSchool'];
 $sessionType      = $variables['SessionType'] == '2Day' ? 2 : 3;
-$pottyTrained     = $variables['PottyTrained'] == 'checked' ? 1 : 0;
-$unaware          = $variables['WaiverChildNameUnware'] == 'checked' ? 1 : 0;
-$aware            = $variables['WaiverChildNameAware'] == 'checked' ? 1 : 0;
-$bus              = $variables['Bus'] == 'checked' ? 1 : 0;
-$water            = $variables['Water']  == 'checked'? 1 : 0;
-$onSite           = $variables['PhotographOnSite'] == 'checked' ? 1 : 0;
-$offSite          = $variables['PhotographOffSite'] == 'checked' ? 1 : 0;
-$medicine         = $variables['Medicine'] == 'checked' ? 1 : 0;
+$pottyTrained     = $variables['PottyTrained'] == 'on' ? 1 : 0;
+$aware            = $variables['Physical'] == 'aware' ? 1 : 0;
+$fieldTrip        = $variables['FieldTrip'] == 'on' ? 1 : 0;
+$water            = $variables['Water']  == 'on'? 1 : 0;
+$onSite           = $variables['PhotographOnSite'] == 'on' ? 1 : 0;
+$offSite          = $variables['PhotographOffSite'] == 'on' ? 1 : 0;
+$medicine         = $variables['Medicine'] == 'on' ? 1 : 0;
 $sign             = sql_safe($variables['Sign']);
+$signedBy         = sql_safe($variables['PrintedName']);
 $eFirst           = sql_safe($variables['EmergencyFirstName']);
 $eLast            = sql_safe($variables['EmergencyLastName']);
 $ePhone           = (int)$variables['EmergencyPhone'];
@@ -45,12 +53,13 @@ $eState           = sql_safe($variables['EmergencyState']);
 $ezip             = (int)$variables['EmergencyZip'];
 $relationship     = sql_safe($variables['EmergencyRelationship']);
 
+$signedDate = date('Y-m-d');
 $saq = "call InsertRegisterSP('$parentFirstName1', '$parentLastName1', '$parentFirstName2',
       '$parentLastName2', '$email',
       $phone, $wPhone ,$cPhone, $checkEmail, '$address', '$city', '$state', '$zip', '$hobbies',
-      '$fname', '$lname', '$nickName', 2017-03-2014, $schoolYear, $gender, $dominantHand,
-      '$additionalInfo', $lengthInSchool, $sessionType, 1, 1, $unaware, $aware, $bus, $water,
-      $onSite, $offSite, $medicine, $pottyTrained, '$eFirst', '$eLast', $ePhone,
+      '$fname', '$lname', '$nickName', '$dob', $schoolYear, $gender, $dominantHand,
+      '$additionalInfo', '$physical', $lengthInSchool, $sessionType, 1, $aware, $fieldTrip, $water,
+      $onSite, $offSite, $medicine, $pottyTrained, '$signedDate', '$signedBy', '$eFirst', '$eLast', $ePhone,
       '$eAddress', '$eCity', '$eState', $ezip, '$relationship')";
 sql_query($saq);
 
