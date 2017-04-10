efe5c24e55526701a8b46661be94d75f50bee379
John Schmidt <john@dgspro.com>
2017-04-10 16:20:08 -0600

    Add correct wording from registration and waiver

--- registration.php
+++ registration.php
@@ -166,16 +166,16 @@ State:
 
 Zip Code:
 <input name="EmergencyZip" type="text" value="<?php echo $var['EmergencyZip']; ?>" required/>
-<h2>I Understand (Please check box)</h2>
-<input name="PottyTrained" type="checkbox" required/> My child must be 3 by July 1st and is FULLY potty trained
+
+<h2>Waiver</h2>
 
 I, <input style="width: 100px;" name="WavierName" type="text" value="<?php echo $var['WaiverName']; ?>" required/> (name parent or legal gaurding), for myself and for my child, agree to all of the following.
 <ol>
  	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
- 	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
- 	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
- 	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
- 	<li>I wish to enroll my child in Love 4 Learning Preschool</li>
+ 	<li>I realize that while attending Love 4 Learning Preschool that my child will be supervised by a teacher or assistant teacher during the time they are at school. I understand that Love 4 Learning Preschool has attempted to create a injury free play area for my child; however, I acknowledge that young children may get hurt while playing with other children and while engaging in physical activities, and that there is a risk of property damage, serious injury or death inherent in my child participating in preschool classes and activities. I also understand that there are risks inherent in any physical activity program, including the use of equipment such as those provided for use at Love 4 Learning Preschool which may or may not be obvious and which may pose serious threats to any person if used improperly.</li>
+ 	<li>In the event my child become injured while participating in activities, I hereby consent to Love 4 Learning Preschool to provide First Aid as well as summing medical professionals to administer First Aid or emergency medical treatment form my child.</li>
+ 	<li>I agree to follow any instructions or rules established by Love 4 Learning Preschool with regard to my child’s activities, whether written or orally given by Love 4 Learning Preschool.</li>
+ 	<li>I understand that every child is in a unique developmental stage and that I am most familiar with my child’s capabilities and limitations. As such, I agree to discuss any concerns with Love 4 Learning Preschool that I have about my child which I think may affect my child’s ability to safely participate in any activities or to use any equipment.</li>
 </ol>
 (Please Check all boxes that apply)
 
@@ -187,54 +187,29 @@ may affect my child's ability to participate in any activity at Love 4 Learning 
 <input name="WaiverChildNameAware" type="text" value="<?php echo $var['WaiverChildNameAware']; ?>" />
 
 <ol start="6">
- 	<li>I agree not to hold Love 4 Learning Preschool, INC as it's entirety
-responsible for any injuries suffered by my child while involved
-in activities at preschool.</li>
- 	<li>I agree not to hold Love 4 Learning Preschool, INC as it's entirety
-responsible for any injuries suffered by my child while involved
-in activities at preschool.</li>
- 	<li>I agree not to hold Love 4 Learning Preschool, INC as it's entirety
-responsible for any injuries suffered by my child while involved
-in activities at preschool.</li>
+ 	<li>I agree not to hold Love 4 Learning Preschool as its entirety responsible for any injuries suffered by my child while involved in activities at preschool.</li>
+ 	<li>I agree to RELEASE, DISCHARGE, IDEMNIFY, PROMISE NOT TO SUE, AND TO SAVE AND HOLD HARMLESS Love 4 Learning Preschool as its entirety form liability, damage, or costs whatsoever arising out of or related to any loss, damage, or injury (including death) to me or my child arising out of or in any way connected with participation in the activities of Love 4 Learning Preschool for any reason or cause.</li>
+ 	<li>I understand and agree that this release and waiver of liability, assumption of risk, and hold harmless agreement is governed by the laws of the State of Utah, and is intended to be as broad and inclusive as is permitted by such law, and that, in the event of any portion of this agreement is determined to be invalid, illegal, or unenforceable, the validity, legality, and enforceability of the balance of the agreement shall not be affected or impaired in any way, and shall continue to full legal force and effect. This release and waiver of liability shall be enforced and interpreted only by binding arbitration in Cache County.</li>
 </ol>
-Parent/Guardian Authorization
+<h2>Parent/Guardian Authorization</h2>
 
 Please list any restrictions to permission of the following:
 
-<input name="FieldTrip" type="checkbox" /> My child may be taken on field trips or excursions by bus or
-private motor vehicle, as well as on neighborhood walking
-excursions under required supervision
+<input name="FieldTrip" type="checkbox" /> My child may be taken on field trips or excursions by bus or private motor vehicle, as well as on neighborhood walking excursions under required supervision
 
-<input name="Water" type="checkbox" /> My child may participate in swimming or other water
-activities under required supervision.
+<input name="Water" type="checkbox" /> My child may participate in swimming or other water activities under required supervision.
 
 My child may be photographed for publicity or news purposes
 <input name="PhotographOnSite" type="checkbox" /> on-site
 <input name="PhotographOffSite" type="checkbox"  /> off-site
 
-<input name="Medicine" type="checkbox" />My child may be given non-prescribed medication as indicated
-on the container. This may include sunscreen, children's pain
-reliever, antibacterial first aid cream, syrup or ipecac may
-be administered if deemed necessary by poison control operator.
-The child's parent or gaurdian will be contacted prior to
-administering non-prescribed pain relievers. Prescription
-medications must be current and a permission slip is required
-per each medication.
+<input name="Medicine" type="checkbox" />My child may be given non-prescribed medication as indicated on the container. This may include sunscreen, children's pain reliever, antibacterial first aid cream, syrup or ipecac may be administered if deemed necessary by poison control operator. The child's parent or gaurdian will be contacted prior to administering non-prescribed pain relievers. Prescription medications must be current and a permission slip is required per each medication.
 
-In an emergency, Love 4 Learning Preschool has my permission
-to call an ambulance, or take my child to any available physician
-or hospital at my expense to obtain medical treatment. In most
-emergencies, 911 is called and the child is transported to the
-nearest hospital and treated by the on-call physician. The
-Parent or gaurdian of the child is notified as soon as possible.
+In an emergency, Love 4 Learning Preschool has my permission to call an ambulance, or take my child to any available physician or hospital at my expense to obtain medical treatment. In most emergencies, 911 is called and the child is transported to the nearest hospital and treated by the on-call physician. The Parent or gaurdian of the child is notified as soon as possible.
 
-I HAVE READ THIS DOCUMENT AND AGREE TO ALL OF ITS TERMS. I
-UNDERSTAND IT IS A LEGALLY BINDING AGREEMENT AND WAIVES CERTAIN
-LEGAL RIGHTS OF MINE, INCLUDING, BUT NOT LIMITED TO A RELEASE,
-WAIVER, PROMISE NOT TO SUE, AND A HOLD HARMLESS FOR ALL CLAIMS.
-THIS AGREEMENT SHALL BE BINDING UPON MYSELF, MY CHILD, AND
-OUR ESTATE, SUCCESSORS AND ASSIGNS.
+I HAVE READ THIS DOCUMENT AND AGREE TO ALL OF ITS TERMS. I UNDERSTAND IT IS A LEGALLY BINDING AGREEMENT AND WAIVES CERTAIN LEGAL RIGHTS OF MINE, INCLUDING, BUT NOT LIMITED TO A RELEASE, WAIVER, PROMISE NOT TO SUE, AND A HOLD HARMLESS FOR ALL CLAIMS. THIS AGREEMENT SHALL BE BINDING UPON MYSELF, MY CHILD, AND OUR ESTATE, SUCCESSORS AND ASSIGNS.
 
+<input name="PottyTrained" type="checkbox" required/> I understand my child must be 3 by July 1st and is FULLY potty trained
 <input name="Sign" type="checkbox" value="1" required/> By checking this box I agree that I have filled out this form to the best of my ability and agree with all that has been stated.
 Parent/Gaurdian Printed Name:
 <input name="PrintedName" type="text" value="<?php echo $var['PrintedName']; ?>" required/>
