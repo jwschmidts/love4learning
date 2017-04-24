e1e303cdc7ad16b14df92d7215b873569da07059
John Schmidt <john@dgspro.com>
2017-04-24 11:53:44 -0600

    Fix birth day drop downs to prefill if provided data

--- registration.php
+++ registration.php
@@ -36,62 +36,43 @@ Two Day Session<input name="SessionType" type="radio" value="2Day" <?php echo ($
 
 Date of Birth:
 <select name="BMonth" required>
-  <option selected disabled>Month</option>
-  <option value='January'>January</option>
-  <option value='February'>February</option>
-  <option value='March'>March</option>
-  <option value='April'>April</option>
-  <option value='May'>May</option>
-  <option value='June'>June</option>
-  <option value='July'>July</option>
-  <option value='August'>August</option>
-  <option value='September'>September</option>
-  <option value='October'>October</option>
-  <option value='November'>November</option>
-  <option value='December'>December</option>
+  <?php
+    if ($var['BMonth'] == '') echo "<option selected disabled>Month</option>";
+    else echo "<option disabled>Month</option>";
+    for ($i = 1; $i<13; $i++)
+    {
+      $dateObj   = DateTime::createFromFormat('!m', $i);
+      $monthName = $dateObj->format('F');
+      $selected  = '';
+      if ($var['BMonth'] == $monthName) $selected = 'selected';
+      echo "<option value=$monthName $selected>$monthName</option>";
+    }
+  ?>
 </select>
 <select name="BDay" required>
-  <option selected disabled>Day</option>
-  <option value='1'>1</option>
-  <option value='2'>2</option>
-  <option value='3'>3</option>
-  <option value='4'>4</option>
-  <option value='5'>5</option>
-  <option value='6'>6</option>
-  <option value='7'>7</option>
-  <option value='8'>8</option>
-  <option value='9'>9</option>
-  <option value='10'>10</option>
-  <option value='11'>11</option>
-  <option value='12'>12</option>
-  <option value='13'>13</option>
-  <option value='14'>14</option>
-  <option value='15'>15</option>
-  <option value='16'>16</option>
-  <option value='17'>17</option>
-  <option value='18'>18</option>
-  <option value='19'>19</option>
-  <option value='20'>20</option>
-  <option value='21'>21</option>
-  <option value='22'>22</option>
-  <option value='23'>23</option>
-  <option value='24'>24</option>
-  <option value='25'>25</option>
-  <option value='26'>26</option>
-  <option value='27'>27</option>
-  <option value='28'>28</option>
-  <option value='29'>29</option>
-  <option value='30'>30</option>
-  <option value='31'>31</option>
+  <?php
+    if ($var['BDay'] == '') echo "<option selected disabled>Day</option>";
+    else echo "<option disabled>Day</option>";
+
+    for ($i = 1; $i<32; $i++)
+    {
+      $selected = '';
+      if ($var['BDay'] == $i) $selected = 'selected';
+      echo "<option value=$i $selected>$i</option>";
+    }
+    ?>
 </select>
 <select name="BYear" required>
-  <option selected disabled>Year</option>
 <?php
+if ($var['BMonth'] == '') echo "<option selected disabled>Year</option>";
+else "<option disabled>Year</option>";
 $currYear = date('Y');
 for ($i=2;$i<7;$i++)
 {
+  $selected = '';
   $d = $currYear - $i;
-  echo "<option value='$d'>$d</option>";
+  if ($var['BYear'] == $d) $selected = 'selected';
+  echo "<option value='$d' $selected>$d</option>";
 }
 ?>
 </select>
